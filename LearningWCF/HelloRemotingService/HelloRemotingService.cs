b236d2f0ed1474ec3a3bf9f7d4ba57d87850ce69
Arslan Ahmad <ars.ahm6@gmail.com>
2019-06-17 09:34:56 +0500

    Hello remoting service implementation added

---
+++ LearningWCF/HelloRemotingService/HelloRemotingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloRemotingService
+{
+    public class HelloRemotingService : MarshalByRefObject, IHelloRemotingService.IHelloRemotingService
+    {
+        public string GetGreetingMessage(string name)
+        {
+            return $"Hello, {name} from remoting service";
+        }
+    }
+}
