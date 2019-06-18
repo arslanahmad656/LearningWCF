8f329644ed1153dae60a39a2b7145713b262d6a4
Arslan Ahmad <ars.ahm6@gmail.com>
2019-06-18 09:42:08 +0500

    WCF service with multiple service contracts added

---
+++ LearningWCF/WcfMultipleServiceContracts/MultipleContractService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfMultipleServiceContracts
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MultipleContractService" in both code and config file together.
+    public class MultipleContractService : IPublicService, IPrivateService
+    {
+        string IPublicService.DisplayMessage()
+        {
+            return "This is meant to be a public service";
+        }
+
+        string IPrivateService.DisplayMessage()
+        {
+            return "This is meant to be a private service";
+        }
+    }
+}
