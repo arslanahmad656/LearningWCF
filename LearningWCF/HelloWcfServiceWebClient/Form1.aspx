cc52264d7fa717fbec340af6680a5e791b297c8e
Arslan Ahmad <ars.ahm6@gmail.com>
2019-06-18 09:36:05 +0500

    Hello wcf service client added

---
+++ LearningWCF/HelloWcfServiceWebClient/Form1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form1.aspx.cs" Inherits="HelloWcfServiceWebClient.Form1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server" Width="471px"></asp:TextBox><asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ LearningWCF/HelloWcfServiceWebClient/Form1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWcfServiceWebClient
+{
+    public partial class Form1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HelloWcfServiceReference.HelloWcfServiceClient client = null;
+            try
+            {
+                client = new HelloWcfServiceReference.HelloWcfServiceClient("http://localhost:56335/HelloService");
+                var message = client.GetMessage(TextBox1.Text);
+                Label1.Text = message;
+            }
+            finally
+            {
+                client?.Close();
+            }
+        }
+    }
+}
