061279d43861ba4316d8960643fdc9386ce74e44
Arslan Ahmad <ars.ahm6@gmail.com>
2019-06-18 10:48:06 +0500

    multiple service contracts service host added

---
+++ LearningWCF/WcfMultipleServiceContractsHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WcfMultipleServiceContracts;
+
+namespace WcfMultipleServiceContractsHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(MultipleContractService)))
+            {
+                host.Open();
+                Console.WriteLine($"Host started");
+                Console.ReadKey();
+            }
+        }
+    }
+}
